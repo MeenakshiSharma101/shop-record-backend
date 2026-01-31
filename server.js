a598ae6b78a3fc29696809bf7220c97373a43378
Meenakshi Sharma <your_email@gmail.com>
2026-01-31 18:30:59 +0530

    modified server.js and db.js

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+import connectDB from "./config/db.js";
+
+connectDB();
+
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
@@ -15,6 +19,8 @@ app.get("/", (req, res) => {
   res.send("Backend working");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
